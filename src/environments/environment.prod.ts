c3aa38380b2068d34e02503d6b2be75aeebc438c
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-11-10 21:12:15 -0500

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {production: true};
 export let APIURL = '';
 
 switch (window.location.hostname) {
-  case 'bootcampfootlocker-client.herokuapp.com':
-    APIURL = 'https://bootcampfootlocker-client.herokuapp.com';
+  case 'kbc-bfserver.herokuapp.com':
+    APIURL = 'https://kbc-bfserver.herokuapp.com';
     break;
   default: APIURL = 'http://localhost:3000';
 }
